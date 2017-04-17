378a8156315c6ab4790cf02b506b8933d10ffbbb
Hiromichi Matsushima <hylom@Hiromichi-no-iMac.local>
2017-04-18 01:42:33 +0900

    implement play/stop

--- ttune/ViewController.swift
+++ ttune/ViewController.swift
@@ -7,26 +7,77 @@
 //
 
 import Cocoa
+import AVFoundation
+
 
 class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource  {
     
     @IBOutlet var contentTableViewController: NSArrayController!
     @IBOutlet weak var contentTableView: NSTableView!
+    @IBOutlet weak var buttonPlayPause: NSButton!
+
+    var isPlaying: Bool = false
+    var playing: TTUContentMO? = nil
+    var player = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         contentTableView.registerForDraggedTypes([NSFilenamesPboardType])
+        isPlaying = false
 
     }
 
+    // UI actions
+    
+    @IBAction func togglePlayPause(sender: AnyObject) {
+        if (isPlaying) {
+            // pause
+            player.stop()
+            isPlaying = false
+            buttonPlayPause.title = "play"
+        } else {
+            // play
+            if let path = playing!.path {
+                let url = NSURL(fileURLWithPath: path)
+                do {
+                    try player = AVAudioPlayer(contentsOfURL: url)
+                    player.prepareToPlay()
+                    player.play()
+                    isPlaying = true
+                    buttonPlayPause.title = "stop"
+                }
+                catch {
+                    isPlaying = false
+                    buttonPlayPause.title = "play"
+                }
+                
+            }
+        }
+        
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
         }
     }
 
+    // tableView delegates
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        if let contents = contentTableViewController.selectedObjects {
+            if contents.count == 1 {
+                playing = contents.first as? TTUContentMO
+            }
+        }
+        if let p = playing {
+            let path = p.path
+            print("selected: \(path)")
+        }
+    }
+    
     func tableView(tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableViewDropOperation) -> NSDragOperation {
         return .Copy
     }
