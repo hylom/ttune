cc7b88745eeaae18c6475d9ad7484e10808bce78
Hiromichi Matsushima <hylom@Hiromichi-no-iMac.local>
2017-05-04 03:47:11 +0900

    use AVAudioPlayerNode instead of AVAudioPlater

--- ttune/ViewController.swift
+++ ttune/ViewController.swift
@@ -11,16 +11,20 @@ import AVFoundation
 
 
 class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource  {
-    
     @IBOutlet var contentTableViewController: NSArrayController!
     @IBOutlet weak var contentTableView: NSTableView!
     @IBOutlet weak var screenView: TTUScreenView!
     @IBOutlet weak var volumeSlider: NSSliderCell!
 
     dynamic var isPlaying: Bool = false
-    var playing: TTUContentMO? = nil
-    var timer: NSTimer? = nil
-    var player: AVAudioPlayer?
+
+    private var nextContent: TTUContentMO? = nil
+    private var currentContent: TTUContentMO? = nil
+    private var timer: NSTimer? = nil
+
+    private var engine: AVAudioEngine = AVAudioEngine()
+    private var playerNode: AVAudioPlayerNode = AVAudioPlayerNode()
+    private var currentFile: AVAudioFile? = nil
     
     override var representedObject: AnyObject? {
         didSet {
@@ -35,7 +39,44 @@ class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSour
         contentTableView.registerForDraggedTypes([NSFilenamesPboardType])
         isPlaying = false
         
-        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+        engine.attachNode(playerNode)
+        let mixer = engine.mainMixerNode
+        engine.connect(playerNode, to: mixer, format: mixer.outputFormatForBus(0))
+        do {
+            try engine.start()
+        } catch {
+            print("AVAudioEngine can't start...")
+        }
+    }
+    
+    private func startPlay() -> Bool {
+        if let next = nextContent {
+            if currentContent == nil || currentContent!.path != next.path {
+                // play next content
+                playerNode.stop()
+                currentContent = nextContent
+                let url = NSURL(fileURLWithPath: next.path)
+                do {
+                    try currentFile = AVAudioFile(forReading: url)
+                    playerNode.scheduleFile(currentFile!, atTime: nil, completionHandler: nil)
+                    playerNode.play()
+                } catch {
+                    return false
+                }
+                return true
+            } else {
+                // resume current content
+                playerNode.play()
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func stopPlay() {
+        playerNode.pause()
+        
     }
     
     func updateTime() {
@@ -43,16 +84,14 @@ class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSour
         formatter.unitsStyle = .Positional
         formatter.zeroFormattingBehavior = .Pad
         formatter.allowedUnits = [.Minute, .Second]
-        if let p = self.player {
-            self.screenView.duration = formatter.stringFromTimeInterval(p.currentTime)!
+        
+        if let t = playerNode.lastRenderTime {
+            if let pt = playerNode.playerTimeForNodeTime(t) {
+                let ct = Double(pt.sampleTime) / Double(pt.sampleRate)
+                self.screenView.duration = formatter.stringFromTimeInterval(NSTimeInterval(ct))!
+            }
         } else {
             self.screenView.duration = formatter.stringFromTimeInterval(0.0)!
-        }
-    }
-    
-    private func initPlayer() {
-        if let p = self.player {
-            p.volume = volumeSlider.floatValue / 100.0
         }
     }
     
@@ -64,24 +103,23 @@ class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSour
 
     // UI actions
     @IBAction func togglePlayState(sender: AnyObject) {
-        if let p = self.player {
-            if (isPlaying) {
-                // play
-                p.play()
-            } else {
-                // pause
-                p.stop()
+        if (isPlaying) {
+            // play
+            if !startPlay() {
+                isPlaying = false
             }
         } else {
-            isPlaying = false;
+            // pause
+            stopPlay()
         }
-        
     }
 
     @IBAction func changeVolume(sender: AnyObject) {
+        /*
         if let p = self.player {
             p.volume = volumeSlider.floatValue / 100.0
         }
+        */
     }
     
     // tableView delegates
@@ -89,18 +127,7 @@ class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSour
         if let contents = contentTableViewController.selectedObjects {
             if contents.count == 1 {
                 if let p = contents.first as? TTUContentMO {
-                    playing = p
-                    screenView.title = p.title
-                    let url = NSURL(fileURLWithPath: p.path)
-                    do {
-                        try player = AVAudioPlayer(contentsOfURL: url)
-                        player!.prepareToPlay()
-                        initPlayer()
-                    }
-                    catch {
-                        isPlaying = false
-                    }
-                    
+                    nextContent = p
                 }
             }
         }
